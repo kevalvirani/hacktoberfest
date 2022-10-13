753d2ef7f8f150e2bb8706a21f33df1d2600e08a
kevalvirani <78257489+kevalvirani@users.noreply.github.com>
2022-10-14 00:07:41 +0530

    updated database file

--- database.py
+++ database.py
@@ -1,25 +1,14 @@
-import sqlite3
-con = sqlite3.connect('vk.db')
-cur = con.cursor()
+import mysql.connector
 
-# Create table
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword"
+)
 
-cur.execute('''CREATE TABLE Bank (account_no real, CustomerName text, Balance real, phone real, address text)''')
+mycursor = mydb.cursor()
 
-# Insert a row of data
-cur.execute("INSERT INTO Bank VALUES (101,'virani keval',40000,9726189535,'Vadodara')")
-cur.execute("INSERT INTO Bank VALUES (102,'sapan shah',30000,1234567890,'jamnagar')")
-cur.execute("INSERT INTO Bank VALUES (103,'uttam pragda',15000,2345678901,'Surat')")
-cur.execute("INSERT INTO Bank VALUES (104,'Kshitij Naria',45000,3456789012,'Surat')")
+mycursor.execute("SHOW DATABASES")
 
-#update a row of data
-cur.execute("UPDATE Bank SET address = 'Mumbai' WHERE account_no = 104")
-
-
-#deletion of a row
-cur.execute("DELETE from Bank WHERE account_no = 103 ")
-
-for row in cur.execute('SELECT * FROM Bank WHERE Balance >= 20000'):
-       print(row)
-# Save (commit) the changes
-con.commit()
+for x in mycursor:
+  print(x)
