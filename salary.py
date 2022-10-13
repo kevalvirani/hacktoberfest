ef34f5f18022bb2a2c02c1ebad73186a6df97866
Yash <65134112+YashPatel-15@users.noreply.github.com>
2022-10-13 12:32:46 +0530

    updated salary file
    Hello, i changed your file in easy way please check and review.

--- salary.py
+++ salary.py
@@ -1,32 +1,13 @@
-class Employee:
-    def __init__(self, employee_id, employee_name, employee_designation, employee_salary):
-        self.employee_id = employee_id
-        self.employee_name = employee_name
-        self.employee_designation = employee_designation
-        self.employee_salary = employee_salary
-    								# inc_sal function
-    def inc_sal(self):
-        if self.employee_designation == "Manager":
-            salary = self.employee_salary + 5000
-            return salary
-        elif self.employee_designation == "General Manager":
-            salary = self.employee_salary + 10000
-            return salary
-        elif self.employee_designation == "CEO":
-            salary = self.employee_salary + 20000
-            return salary
-        elif self.employee_designation == "worker":
-            salary = self.employee_salary + 2000
-            return salary
-# taking_input
-e_id = int(input("Enter the Employee ID : "))
-e_name = input("Enter the Employee Name : ")
-e_designation = input("Enter the Employee Designation : ")
-e_salary = int(input("Enter the Employee Salary : "))
-# creating obj of Employee class
-emp_obj = Employee(e_id, e_name, e_designation, e_salary)
-# getting access of inc_sal function
-salary = emp_obj.inc_sal()
-print("Incremental Employee Salary is : ", salary)
-# another way
-print("Incremental Employee Salary is : %d" % salary)
+def weeklyPaid(hours_worked, wage):
+    if hours_worked > 40:
+        return 40 * wage + (hours_worked - 40) * wage * 1.5
+    else:
+        return hours_worked * wage
+ 
+ 
+hours_worked = 50
+wage = 100
+ 
+pay = weeklyPaid(hours_worked, wage)
+ 
+print(f"Total gross pay: Rs.{pay:.2f} ")
