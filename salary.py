f31c40dcfd09a3843b5caf2a90de7b6b3fe960ec
Jaydeep5550 <78203017+Jaydeep5550@users.noreply.github.com>
2022-10-13 23:26:46 +0530

    Updated file
    Hello Yash i have made some changes to your file please check and let me know

--- salary.py
+++ salary.py
@@ -1,32 +1,31 @@
-class Employee:
-    def __init__(self, employee_id, employee_name, employee_designation, employee_salary):
-        self.employee_id = employee_id
-        self.employee_name = employee_name
-        self.employee_designation = employee_designation
-        self.employee_salary = employee_salary
-    								# inc_sal function
-    def inc_sal(self):
-        if self.employee_designation == "Manager":
-            salary = self.employee_salary + 5000
-            return salary
-        elif self.employee_designation == "General Manager":
-            salary = self.employee_salary + 10000
-            return salary
-        elif self.employee_designation == "CEO":
-            salary = self.employee_salary + 20000
-            return salary
-        elif self.employee_designation == "worker":
-            salary = self.employee_salary + 2000
-            return salary
-# taking_input
-e_id = int(input("Enter the Employee ID : "))
-e_name = input("Enter the Employee Name : ")
-e_designation = input("Enter the Employee Designation : ")
-e_salary = int(input("Enter the Employee Salary : "))
-# creating obj of Employee class
-emp_obj = Employee(e_id, e_name, e_designation, e_salary)
-# getting access of inc_sal function
-salary = emp_obj.inc_sal()
-print("Incremental Employee Salary is : ", salary)
-# another way
-print("Incremental Employee Salary is : %d" % salary)
+print ("MENU")
+print ()
+print ("B - Burger ")
+print("F - French Fries")
+print("P - Pizza ")
+print("S - Sandwich")
+print ()
+Burger= 0
+FrenchFries= 0
+Pizza= 0
+Sandwich= 0
+
+x=1
+
+while x==1:
+    i = input("Enter the food that you want to buy: ")
+    if i  == "B":
+        Burger+=200
+        print ("Burger ------------Php. 200.00")
+    elif i == "F" :
+        FrenchFries+=50
+        print ("FrenchFries ------------Php. 50.00")
+    elif i == "P":
+         Pizza+=500
+         print ("Pizza ------------Php. 500.00")
+    elif i == "S":
+         Sandwich+=150
+         print ("Sandwich ---------Php. 150.00")
+    x=int(input("Do you still want to buy anything? 1 if yes 0 if no "))
+total= Burger + FrenchFries + Pizza + Sandwich   
+print("Total: ", total)
